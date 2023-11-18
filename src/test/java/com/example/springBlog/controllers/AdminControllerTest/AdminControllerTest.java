3ad49ee48b16a5ff245f2bf61d3507daa844bbdc
Tework123 <daniliesaec@gmail.com>
2023-11-18 07:44:42 +0300

    delete tests

--- src/test/java/com/example/springBlog/controllers/AdminControllerTest/AdminControllerTest.java
+++
@@ -1,9 +0,0 @@
-package com.example.springBlog.controllers.AdminControllerTest;
-
-public class AdminControllerTest {
-//    тесты на:
-//    список юзеров
-//    активация/бан юзеров
-//    изменение роли
-//    также защищенный доступ к /admin только с ролью ADMIN
-}
--- src/test/java/com/example/springBlog/controllers/UserProfileTest/UserProfileControllerTest.java
+++
@@ -1,9 +0,0 @@
-package com.example.springBlog.controllers.UserProfileTest;
-
-public class UserProfileControllerTest {
-    //    тесты на:
-//      профиль юзера
-//      изменение юзера(только для owner)
-//      удаление юзера(только для owner)
-//      функционал с  followers
-}
